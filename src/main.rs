94578ca21bd255486cab6b91ec206ed377493e93
Paulo Matias <matias@ufscar.br>
2024-04-15 15:31:18 -0300

    add command line options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use anyhow;
+use clap::Parser;
 
 #[cfg_attr(unix, path = "serial_linux.rs")]
 #[cfg_attr(windows, path = "serial_windows.rs")]
 mod serial;
 
+#[derive(Parser, Debug)]
+#[command(version, about = "Dial-up modem", long_about = None)]
+struct Opt {
+    /// Audio device to use for RX
+    #[arg(short, long, default_value_t = String::from("default"))]
+    rxdev: String,
+
+    /// Audio device to use for TX
+    #[arg(short, long, default_value_t = String::from("default"))]
+    txdev: String,
+
+    /// Serial device (Windows-only)
+    #[arg(short, long, default_value_t = String::from("\\\\.\\COM3"))]
+    serdev: String,
+}
+
 fn main() -> anyhow::Result<()> {
-    let mut serial = serial::Serial::open("", Box::new(|b| println!("received {}", b)))?;
+    let opt = Opt::parse();
+    let mut serial = serial::Serial::open(&opt.serdev, Box::new(|b| println!("received {}", b)))?;
     serial.event_loop()?;
     Ok(())
 }
